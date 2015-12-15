4e466547d203bea4883e5422451c052807b4ae5e
Mateus Sobrinho <chocsx@hotmail.com>
2015-12-15 02:00:37 -0200

    Add PHP version

---
+++ php/WesleySafadao.php
@@ -0,0 +1,16 @@
+<?php
+
+function wesley_safadao($dia, $mes, $ano){
+	$sum = 0;	
+	while ($mes > 0)
+      {
+        $sum = $sum + $mes;
+        $mes--;
+      }
+	  	
+	$safadeza = $sum + ($ano / 100) * (50 - $dia);
+	$anjo = 100 - $safadeza;
+	
+	return "Você é ".$safadeza."% safado e ".$anjo."% anjo";
+	
+}
