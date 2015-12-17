995b7f3b0698fa9eac14efcd5e4a7279dcaab3e1
abraaoalves <abraao.teodosio@gmail.com>
2015-12-16 22:27:20 -0300

    remove verbose syntax

--- typescript/version-2/wesleySafadao.ts
+++ typescript/version-2/wesleySafadao.ts
@@ -1,15 +1,8 @@
 var sum = (val:number):number => val>1 ? val + sum(val - 1) : 1;
 
 export class Safadao {
-	day:number;
-	month:number;
-	year:number;
 
-	constructor(day:number, month:number, year:number) {
-		this.day = day;
-		this.month = month;
-		this.year = year;
-	}
+	constructor(public day:number, public month:number, public year:number) {}
 
 	safadeza():string {
 		return `${this._angel().toFixed(1)}% anjo, perfeito ` +
