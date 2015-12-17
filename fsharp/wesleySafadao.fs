26de9ac12757d1ab56507078857697242284c3d6
Marcelo Camargo <marcelocamargo@linuxmail.org>
2015-12-17 08:16:46 -0200

    Fixed F# code style and angel return

--- fsharp/wesleySafadao.fs
+++ fsharp/wesleySafadao.fs
@@ -5,10 +5,10 @@ let rec sum x = function
   | hd :: nl -> sum (hd + x) nl
 
 let wsCalculos day month year = 
-  let _naughty = (sum 0 [1..month] |> double) + ((year|>double) / 100.0) * (50.0 - (day|>double))
+  let _naughty = (sum 0 [1..month] |> double) + ((year |> double) / 100.0) * (50.0 - (day |> double))
   let _angel = 100.0 - _naughty
   
-  _naughty, _naughty
+  _naughty, _angel
 
 let safadeza tuple =
   let (x,y) = tuple
