6eeca65e4d41acc4385e9f3a8319f896ca4e6b51
Rodrigo Prado <royopa@gmail.com>
2016-03-11 23:43:12 -0300

    applied psr2 rules

--- php/WesleySafadao.php
+++ php/WesleySafadao.php
@@ -1,7 +1,8 @@
 <?php
 
-function wesley_safadao($day, $month, $year) {
-  $safadeza = array_sum(range(1, $month)) + ($year / 100) * (50 - $day);
-  $anjo = 100 - $safadeza;
-  echo "Você é {$safadeza}% safado e {$anjo}% anjo";
+function wesley_safadao($day, $month, $year)
+{
+    $safadeza = array_sum(range(1, $month)) + ($year / 100) * (50 - $day);
+    $anjo = 100 - $safadeza;
+    echo "Você é {$safadeza}% safado e {$anjo}% anjo";
 }
